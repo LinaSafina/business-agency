f1790071af6dcefd966b2f7ed9e44635fbd01ac8
Alina Safina <lina.zumba@mail.ru>
2022-11-30 17:11:46 +0300

    feat: finish adding animation for the header

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Header
+const header = document.querySelector('.header');
+const burger = document.querySelector('.burger');
+const navListItems = document.querySelectorAll('.nav__list-item');
 
+const toggleActiveClass = () => {
+  header.classList.toggle('active');
+};
+
+burger.addEventListener('click', toggleActiveClass);
+
+navListItems.forEach((item) =>
+  item.addEventListener('click', toggleActiveClass)
+);
+// End of Header
