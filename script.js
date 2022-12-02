52aae6af857a0f8e0cd9b83bb6a24613feef75f0
Alina Safina <lina.zumba@mail.ru>
2022-12-02 06:36:59 +0300

    feat: finish adding custom controls to the video

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ navListItems.forEach((item) =>
   item.addEventListener('click', toggleActiveClass)
 );
 // End of Header
+
+// Video Controls
+const video = document.querySelector('.video');
+const videoButton = document.querySelector('.video__button i');
+const videoBar = document.querySelector('.video__bar');
+
+const playPauseVideo = () => {
+  if (video.paused) {
+    video.play();
+    videoButton.className = 'fa-regular fa-circle-play';
+    video.style.opacity = '0.7';
+  } else {
+    video.pause();
+    videoButton.className = 'fa-regular fa-circle-pause';
+    video.style.opacity = '0.3';
+  }
+
+  videoButton.classList.toggle('fa-circle-pause');
+  videoButton.classList.toggle('fa-circle-play');
+};
+
+video.addEventListener('timeupdate', () => {
+  const { currentTime, duration, ended } = video;
+
+  const playedPercent = (currentTime / duration) * 100;
+  videoBar.style.width = `${playedPercent}%`;
+
+  if (ended) {
+    videoButton.className = 'fa-regular fa-circle-play';
+    video.style.opacity = '0.3';
+  }
+});
+
+videoButton.addEventListener('click', playPauseVideo);
+// End of Video Controls
